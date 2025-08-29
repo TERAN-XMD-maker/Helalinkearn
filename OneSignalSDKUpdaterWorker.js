1453c2d45d7a357a9aa7be68e01babbb09e60d72
TERAN-XMD-maker <teranxd11@gmail.com>
2025-08-29 22:00:22 +0300

    Update pushengage-sw.js

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,3 +1,3 @@
 // pushengage-sw.js — put this at site root (e.g. https://yourdomain.com/pushengage-sw.js)
 // This imports the official PushEngage worker used to show notifications.
-importScripts("https://clientcdn.pushengage.com/sdks/service-worker.js");
+importScripts("https://clientcdn.pushengage.com/sdks/service-worker.js"); 
